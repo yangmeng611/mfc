50151d7581119f37207953ed4e65fbce80186696
yangmeng611 <1558647190@qq.com>
2020-02-29 12:19:13 +0800

    springboot+mybatis环境搭建完毕

---
+++ src/main/java/com/ncu/mfc/MfcApplication.java
@@ -0,0 +1,13 @@
+package com.ncu.mfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ncu/mfc/MfcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncu.mfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
